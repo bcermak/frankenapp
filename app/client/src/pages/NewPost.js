d10ee632a1ef28489ddcf22c5124420a70c81e94
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-26 20:31:57 -0500

    getting rid of post form

--- app/client/src/pages/NewPost.js
+++ app/client/src/pages/NewPost.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Nav from '../components/Nav/Nav';
-import PostForm from '../components/PostForm';
 import Footer from '../components/Footer';
 
 const NewPost = () => {
     return (
         <div className="container">
             <Nav />
-            <PostForm /> 
+
             <Footer /> 
         </div>
     );
