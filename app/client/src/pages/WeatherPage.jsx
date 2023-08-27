2e0ab8ecfeb3887716c0eaf5f2c57e154523d05b
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-26 20:25:46 -0500

    fixing button styling/new css folder structure

--- app/client/src/pages/WeatherPage.jsx
+++ app/client/src/pages/WeatherPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from '../components/Nav/Nav';
 import Footer from '../components/Footer';
+import '../assets/css/Weatherpage.css';
 
 
 const WeatherPage = () => {
@@ -10,7 +11,7 @@ const WeatherPage = () => {
             <div className='row'>
                 <div className="col-md-12" style={{textAlign: 'center', padding: '33px'}}>
                     <input type="text" placeholder='city name'/>
-                    <button style={{backgroundColor: 'rebeccapurple', marginLeft: '10px', padding:'4px'}}> Search Weather </button>
+                    <button className='btn'> Weather button</button>
                 </div>
             </div>
             <div className="row">
