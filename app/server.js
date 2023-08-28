29160250105078aadb5000498adc65a8ee711038
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-28 18:34:20 -0500

    hiding URI

--- app/server.js
+++ app/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const routes = require('./routes/api')
-
+require('dotenv').config();
+const routes = require('./routes/api');
 const bodyParser = require("body-parser") // self-explanatory 
+
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 
@@ -13,7 +14,7 @@ app.use('/api/', routes);
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect("mongodb+srv://bcermak:Skylar-10@triptails.kmoorxl.mongodb.net/TriptailsDB")
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`Database connected ${conn.connection.host}`)
 
     } catch (error) {
