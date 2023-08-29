45a63b7cf3572ad2e4d9790b9a88dd66874ac611
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-28 19:58:22 -0500

    buttoon

--- app/client/src/components/JournalEntry.js
+++ app/client/src/components/JournalEntry.js
@@ -3,14 +3,16 @@ import './css/JournalEntry.css'
 
 const JournalEntry = () => {
   return (
-    <div className='formArea'>
-        <div className='entryArea'>
-            <div class="textBox mb-3">
-                <label for="journalText" class="form-label">Journal Entry</label>
-                <textarea class="form-control" rows="10"></textarea>
-            </div>
-            <button className='btn'>Submit entry</button>
-        </div>
+    <div className='container'>
+      <div className='formArea'>
+          <div className='entryArea'>
+              <div class="textBox mb-3">
+                  <label for="journalText" class="form-label">Journal Entry</label>
+                  <textarea class="form-control" rows="10"></textarea>
+              </div>          
+          </div>
+      </div>
+      <button className='entryBtn'>Submit entry</button>
     </div>
   )
 }
